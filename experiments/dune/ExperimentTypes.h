e372755450b63a2b38a0ad005108f9dfa5d96cc6
Nived Puthumana Meleppattu <nivedpm@dunegpvm02.fnal.gov>
2025-12-17 05:37:25 -0600

    add pdvd crthit

--- experiments/dune/ExperimentTypes.h
+++ experiments/dune/ExperimentTypes.h
@@ -1,11 +1,10 @@
 #ifndef __SUPERA_EXPERIMENT_TYPES_CXX__
 #define __SUPERA_EXPERIMENT_TYPES_CXX__
-//ProtoDUNE CRT includes (DUNE has no CRT)
-#include "duneprototypes/Protodune/singlephase/CRT/alg/monitor/OnlinePlotter.cpp"
-#include "duneprototypes/Protodune/singlephase/CRT/alg/util/FlatDirectory.cpp"
+
+#include "duneprototypes/Protodune/vd/CRT/CRTHit.hh"
 
 namespace supera {
-	typedef CRT::Hit   LArCRTHit_t;
+        typedef dune::crt::CRTHit   LArCRTHit_t;
 }
 
 #endif
